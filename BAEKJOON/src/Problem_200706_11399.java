7942411d31766368b35b00946ea59b784889c83e
lee byeonghoon <qudgns200@gmail.com>
2020-07-07 23:13:40 +0900

    upload source

--- BAEKJOON/src/Problem_200706_11399.java
+++ BAEKJOON/src/Problem_200706_11399.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Programmers_20200706_kakao_2019_1 {
+public class Problem_200706_11399 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
